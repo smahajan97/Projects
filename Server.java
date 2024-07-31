0234c393170bfdc0efb61412d45d19cd8d2f2129
smahajan97 <136894004+smahajan97@users.noreply.github.com>
2024-07-31 14:48:17 +0530

    Create Server.java
    A server-client messaging application in Java using socket programming.  The server listens for client connections on a specific port and facilitates two-way communication between the client and the server.

---
+++ Server.java
@@ -0,0 +1,38 @@
+import java.net.*;
+import java.io.*;
+
+class Server
+{
+    public static void main(String Arg[]) throws Exception
+    {
+        System.out.println("Server application is running...");
+        
+        ServerSocket ssobj = new ServerSocket(2100);
+        System.out.println("Server is waiting for client...");
+
+        Socket sobj = ssobj.accept();
+        System.out.println("Server gets succesfully connected...");
+
+        PrintStream ps = new PrintStream(sobj.getOutputStream());
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(sobj.getInputStream()));
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+        String str1, str2;
+
+        System.out.println("Messanger Application started...");
+
+        while((str1 = br1.readLine()) != null)
+        {
+            System.out.println("Client says : "+str1);
+            if(str1.equalsIgnoreCase("exit"))
+            {
+                break;
+            }
+            System.out.println("Enter message for client : ");
+            str2 = br2.readLine();
+            ps.println(str2);
+        }
+        System.out.println("Thank you for using Massanger Application...");
+        sobj.close();
+        ssobj.close();
+    }
+}
